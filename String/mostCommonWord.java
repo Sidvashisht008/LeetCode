46f821e78d813fca2eaa05fe1e0795905c87b714
Siddharth Vashisht <svashisht008@gmail.com>
2020-07-29 22:48:03 +0530

    Update mostCommonWord.java

--- String/mostCommonWord.java
+++ String/mostCommonWord.java
@@ -20,3 +20,37 @@ class Solution {
         
     }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Map;
+import java.util.Map.Entry;
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.replaceAll("[^a-zA-Z0-9 ]"," ");
+        HashMap<String,Integer> hmap = new HashMap<>();
+        String[] para = paragraph.split("\\s+");
+        for(int i = 0;i<para.length;i++){
+            para[i] = para[i].toLowerCase();
+            if(hmap.containsKey(para[i])){
+                Integer c = hmap.get(para[i]);
+                hmap.put(para[i],c+1);
+            }else{
+                hmap.put(para[i],1);
+            }
+        }
+        for(int i = 0;i<banned.length;i++){
+            hmap.remove(banned[i]);
+        }
+        int maxValue = -1;
+        String maxKey = "";
+        for(Entry <String,Integer> val: hmap.entrySet()){
+            if(val.getValue()>maxValue){
+                maxKey = val.getKey();
+                maxValue = val.getValue(); 
+            }
+        }
+        return maxKey;
+    }
+}
