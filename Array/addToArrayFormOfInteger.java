05df8de08de6f101ef5164b1d7ef2e1f45327e19
Siddharth Vashisht <svashisht008@gmail.com>
2020-09-09 19:25:29 +0530

    Create addToArrayFormOfInteger.java

---
+++ Array/addToArrayFormOfInteger.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        LinkedList<Integer> res = new LinkedList<>();
+        int i = A.length-1;
+        int carry = 0;
+        while(K>0){
+            int sum = K%10 +carry;
+            if(i>=0){
+                sum += A[i];    
+            }
+            carry = sum/10;
+            sum = sum%10;
+            res.addFirst(sum);
+            i--;
+            K/=10;
+        }
+        while(i>=0){
+            int sum = A[i] + carry;
+            carry = sum/10;
+            sum = sum%10;
+            i--;
+            res.addFirst(sum);
+        }
+        if(carry > 0){
+            res.addFirst(carry);
+        }
+        return res;
+    }
+}
