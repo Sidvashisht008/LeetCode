07410e01c4ee4556032694341a7370b2c4d50c74
Siddharth Vashisht <svashisht008@gmail.com>
2020-09-14 21:45:45 +0530

    Update BalanceBinaryTree.java

--- Tree/BalanceBinaryTree.java
+++ Tree/BalanceBinaryTree.java
@@ -14,25 +14,6 @@
  * }
  */
  
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(root ==null){
-            return true;
-        }
-        boolean lb = isBalanced(root.left);
-        boolean rb = isBalanced(root.right);
-        int bf = height(root.left)-height(root.right);
-        return lb && rb &&(bf == 0 || bf == 1 || bf == -1);
-    }
-    public int height(TreeNode node){
-        if(node == null){
-            return -1;
-        }
-        int lh = height(node.left);
-        int rh =height(node.right);
-        return Math.max(lh,rh)+1;
-    }
-}
 
 
 
