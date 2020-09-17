a10d29adabc249ab97eeeab3bd034292de36e056
Siddharth Vashisht <svashisht008@gmail.com>
2020-09-17 23:35:56 +0530

    Create romanToInteger.java

---
+++ String/romanToInteger.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int romanToInt(String s){
+        int[] map = new int[26];
+        map['I' - 'A'] = 1;
+        map['V' - 'A'] = 5;
+        map['X' - 'A'] = 10;
+        map['L' - 'A'] = 50;
+        map['C' - 'A'] = 100;
+        map['D' - 'A'] = 500;
+        map['M' - 'A'] = 1000;
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(i > 0 && map[s.charAt(i - 1) - 'A'] < map[s.charAt(i) - 'A'])
+                result += map[s.charAt(i) - 'A'] - (2 * map[s.charAt(i - 1) - 'A']);
+            else
+                result += map[s.charAt(i) - 'A'];
+        }
+        return result;
+    }
+}
