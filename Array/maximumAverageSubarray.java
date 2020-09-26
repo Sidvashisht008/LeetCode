88123dfaaf86051057d82efc00ed43eb449b7e57
Siddharth Vashisht <svashisht008@gmail.com>
2020-09-26 20:03:59 +0530

    Update maximumAverageSubarray.java

--- Array/maximumAverageSubarray.java
+++ Array/maximumAverageSubarray.java
@@ -15,3 +15,29 @@ class Solution {
         return avg;
     }
 }
+
+
+// Improved Method
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum+=nums[i];
+            nums[i] = sum;
+        }
+        double avg = Integer.MIN_VALUE;
+        int j = -1;
+        for(int i = k-1;i<nums.length;i++){
+            if(j<0){
+                avg = nums[i];
+            }else{
+                if(nums[i]-nums[j]>avg){
+                    avg = nums[i] - nums[j];
+                }
+            }
+            j++;
+        }
+        return avg/k;
+    }
+}
