72d931dd0792257415b28712605738ad94f509c3
Siddharth Vashisht <svashisht008@gmail.com>
2020-10-06 18:37:11 +0530

    Create findPositiveSolutionOfEqn.java

---
+++ Ambigious/findPositiveSolutionOfEqn.java
@@ -0,0 +1,31 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     public int f(int x, int y);
+ * };
+ */
+
+class Solution {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> out=new ArrayList();
+        for(int i=1;i<=z;i++){
+              for(int j=1;j<=z;j++){
+                  int res=customfunction.f(i,j);
+                  if(res>z){
+                      break;
+                  }
+                  if(res==z){
+                      List<Integer> list=new ArrayList();
+                      list.add(i);
+                      list.add(j);
+                      out.add(list);
+                  }
+              }   
+        }
+        return out;
+    }
+}
